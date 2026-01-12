c5ebe7bb57ad84e9cdbd8ae511dd802491b0e001
Nando <donnarummanando@gmail.com>
2026-01-12 10:42:30 +0100

    Initial commit

---
+++ src/testGit/Avvio.java
@@ -0,0 +1,12 @@
+package testGit;
+
+public class Avvio {
+
+	public static void main(String[] args) {
+		System.out.println("test git");
+		System.out.println("il buon Mattia");
+		System.out.println("la buon Francesca");
+
+	}
+
+}
